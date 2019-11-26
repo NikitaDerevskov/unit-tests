e9725bd4da754a67eb116d4a08d4a2abb1850a84
Nikita Derevskov <n.derevskov@examus.net>
2019-11-26 20:25:28 +0500

    intervals: start to do flattenIntervals

--- flatten-intervals.js
+++ flatten-intervals.js
@@ -20,11 +20,34 @@ let flattenIntervals2 = (a, b) => {
     : [first, second]
 }
 
+let flattenIntervals = x => {
+  let sorted = x.sort((a, b) => startOf(a) - startOf(b))
+  return sorted.reduce((acc, y, i) => {
+    if (i == 0) {
+      let res = flattenIntervals2(y, sorted[i+1])
+      return res.length === 1
+      ? acc.concat(res)
+      : acc.concat(y)
+    } else if (i > 0) {
+      let res = flattenIntervals2(y, sorted[i-1])
+      console.log('YYYY', y)
+      console.log('RESS', res[0])
+      return acc
+    }
+  },[])
+}
+
 /* */
+
+let a = [[15, 100], [2, 5], [3, 10]]
+
+// console.log(flattenIntervals2([2, 5], [3,10]))
+console.log('flattenIntervals', flattenIntervals(a))
+
 
 module.exports = {
   _sorted2: sorted2,
   _startOf: startOf,
   _endOf: endOf,
-  /* flattenIntervals, */
+  flattenIntervals,
   flattenIntervals2}
