96a16173c92c7b05046e211b4d3de91ff34aa534
shofel <vistomin@examus.net>
2019-11-26 18:20:14 +0500

    startOf, endOf, sorted

--- flatten-intervals.js
+++ flatten-intervals.js
@@ -1,3 +1,24 @@
-module.exports = {
-  f: _ => true
+/**
+ *
+ */
+
+let sorted2 = ([a, b]) =>
+  startOf(a) > startOf(b)
+    ? [b, a]
+    : [a, b]
+
+let startOf = ([x, _]) => x
+let endOf = ([_, x]) => x
+
+let flattenIntervals2 = (a, b) => {
+  let [first, second] = sorted([a, b])
 }
+
+/* */
+
+module.exports = {
+  _sorted2: sorted2,
+  _startOf: startOf,
+  _endOf: endOf,
+  /* flattenIntervals, */
+  flattenIntervals2}
--- test.js
+++ test.js
@@ -1,7 +1,42 @@
-import {f} from './flatten-intervals'
+import * as m from './flatten-intervals'
 
 import test from 'ava'
 
-test('f returns true', t => {
-  t.is(f(), true)
+/* */
+
+let testf = f => {
+  let macro = (t, input, expected) =>
+    t.deepEqual(f(input), expected)
+
+  macro.title =
+    (f = '', input, expected) =>
+      `${f}(${input}) -> ${expected}`
+
+  return macro
+}
+
+for (let [input, expected] of [
+  [[1, 2], 1],
+  [[2, 1], 2],
+  [[3], 3],
+  [[], undefined]
+]) {
+  test('startOf', testf(m._startOf), input, expected)
+}
+
+for (let [input, expected] of [
+  [[1, 2], 2],
+  [[2, 1], 1],
+  [[1, 1], 1],
+  [[3], undefined],
+  [[], undefined]
+]) {
+  test('endOf', testf(m._endOf), input, expected)
+}
+
+/* flattenIntervals2 */
+
+test.skip('flatten intervals for two args', t => {
+  t.deepEqual(m.flattenIntervals2(), )
+  t.deepEqual(m.flattenIntervals2(), )
 })
