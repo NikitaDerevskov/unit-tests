1dfdfb1ffd107789ab9bbff43c395f2f2ec1b9d5
Nikita Derevskov <n.derevskov@examus.net>
2019-11-27 17:07:21 +0500

    intervals: delete trash and etc

--- flatten-intervals.js
+++ flatten-intervals.js
@@ -5,23 +5,19 @@
 let startOf = ([x, _]) => x
 let endOf = ([_, x]) => x
 
-let sorted2 = (a, b) =>
-  startOf(a) > startOf(b)
-    ? [b, a]
-    : [a, b]
-
 let merge = (a, b) => [
       Math.min(...[a, b].map(startOf)),
       Math.max(...[a, b].map(endOf))]
 
 let flattenIntervals = x => {
+  /* Sort intervals by their starts.*/
   let sorted = x.sort((a, b) => startOf(a) - startOf(b))
 
-  let step = (res, [f, s, ...rest]) => {
-    if (!s) return [...res, f]
-    return endOf(f) >= startOf(s)
-    ? step(res, [merge(f, s), ...rest])
-    : step([...res, f], [s,...rest])
+  let step = (acc, [f, s, ...rest]) => {
+    if (!s) return [...acc, f] // f is the last
+    return endOf(f) >= startOf(s) // when instersect
+      ? step(acc, [merge(f, s), ...rest])
+      : step([...acc, f], [s,...rest])
   }
 
   return step([], sorted)
@@ -30,7 +26,6 @@ let flattenIntervals = x => {
 /* */
 
 module.exports = {
-  _sorted2: sorted2,
   _startOf: startOf,
   _endOf: endOf,
   _merge: merge,
